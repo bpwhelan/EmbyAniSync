774203269b7308025a4b14204aabe3a4d825e13c
Beannsss <bpwhelan95@gmail.com>
2022-10-25 13:01:16 -0400

    Fixes and changes
    Fixed custom mapping to be inline with forked repo.
    
    Added example scripter.xml to help with configuring emby plugin Scripter-X
    
    Some more naming stuff

--- TautulliSyncHelper.py
+++ TautulliSyncHelper.py
@@ -6,14 +6,14 @@ from time import sleep
 
 import coloredlogs
 
-from custom_mappings import read_custom_mappings
 import anilist
-import plexmodule
+import embymodule
 import graphql
 from _version import __version__
+from custom_mappings import read_custom_mappings
 
 # Logger settings
-logger = logging.getLogger("PlexAniSync")
+logger = logging.getLogger("EmbyAniSync")
 coloredlogs.install(fmt="%(asctime)s %(message)s", logger=logger)
 
 
@@ -49,16 +49,16 @@ else:
 
 settings = read_settings(SETTINGS_FILE)
 anilist_settings = settings["ANILIST"]
-plex_settings = settings["PLEX"]
+emby_settings = settings["EMBY"]
 
 graphql.ANILIST_ACCESS_TOKEN = anilist_settings["access_token"].strip()
 
 if "skip_list_update" in anilist_settings:
     graphql.ANILIST_SKIP_UPDATE = anilist_settings["skip_list_update"].lower().strip() == "true"
 
-if "plex_episode_count_priority" in anilist_settings:
-    anilist.ANILIST_PLEX_EPISODE_COUNT_PRIORITY = (
-        anilist_settings["plex_episode_count_priority"].lower().strip() == "true"
+if "emby_episode_count_priority" in anilist_settings:
+    anilist.ANILIST_EMBY_EPISODE_COUNT_PRIORITY = (
+        anilist_settings["emby_episode_count_priority"].lower().strip() == "true"
     )
 
 if "log_failed_matches" in anilist_settings:
@@ -69,7 +69,7 @@ if "log_failed_matches" in anilist_settings:
 
 ## Startup section ##
 def start():
-    logger.info(f"PlexAniSync - version: {__version__}")
+    logger.info(f"EmbyAniSync - version: {__version__}")
     logger.info(f"Updating single show: {show_title}")
 
     anilist.CUSTOM_MAPPINGS = read_custom_mappings()
@@ -85,7 +85,7 @@ def start():
     anilist_username = anilist_settings["username"]
     anilist_series = anilist.process_user_list(anilist_username)
 
-    # Plex
+    # Emby
     if anilist_series is None:
         logger.error(
             "Unable to retrieve AniList list, check your username and access token"
@@ -93,23 +93,23 @@ def start():
     elif not anilist_series:
         logger.error("No items found on your AniList list to process")
     else:
-        # Wait a few a seconds to make sure Plex has processed watched states
+        # Wait a few a seconds to make sure Emby has processed watched states
         sleep(5.0)
-        plexmodule.plex_settings = plex_settings
-        plex_anime_series = plexmodule.get_anime_shows_filter(show_title)
+        embymodule.emby_settings = emby_settings
+        emby_anime_series = embymodule.get_anime_shows_filter(show_title)
 
-        if plex_anime_series is None:
-            logger.error("Found no Plex shows for processing")
-            plex_series_watched = None
+        if emby_anime_series is None:
+            logger.error("Found no Emby shows for processing")
+            emby_series_watched = None
         else:
-            plex_series_watched = plexmodule.get_watched_shows(plex_anime_series)
+            emby_series_watched = embymodule.get_watched_shows(emby_anime_series)
 
-        if plex_series_watched is None:
-            logger.error("Found no watched shows on Plex for processing")
+        if emby_series_watched is None:
+            logger.error("Found no watched shows on Emby for processing")
         else:
-            anilist.match_to_plex(anilist_series, plex_series_watched)
+            anilist.match_to_emby(anilist_series, emby_series_watched)
 
-        logger.info("Plex to AniList sync finished")
+        logger.info("Emby to AniList sync finished")
 
 
 if __name__ == "__main__":
---
+++ runSync.bat
@@ -0,0 +1,2 @@
+@ECHO OFF
+"C:\Users\Beans\Python Environments\EmbyAniSync\Scripts\python.exe" C:/Users/Beans/PycharmProjects/EmbyAniSync/EmbyAniSync.py
